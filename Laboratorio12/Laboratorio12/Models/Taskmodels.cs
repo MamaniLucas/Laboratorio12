ad4cf7700d11b686eb88c489f289442fc00a005b
Alumno <Alumno@LAB1509-08>
2023-12-01 17:51:38 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio12/Laboratorio12/Models/Taskmodels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratorio12.Models
+{
+    public class Taskmodels
+    {
+        //public 
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string IsCompleted { get; set; }
+
+    }
+}
---
+++ Laboratorio12/Laboratorio12/ViewModels/TaskViewModel.cs
@@ -0,0 +1,75 @@
+﻿using Laboratorio12.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Laboratorio12.ViewModels
+{
+    public class TaskViewModel : ViewModelsBasic
+    {
+
+        //definen las propiedades 
+        //de la vista xaml.c al tipo lista
+        //tipo de coleccion , porpiedad de tipo task.
+        // lo  mismo que title set y get
+
+        //que propiedades son 
+        //modelos=objetos de la base de datos
+        //modelo que simula la infor de la bd
+        //rn odel creo una clase de tipo taskmodels
+
+
+
+        //Configura el ListView para que muestre el título y la descripción de cada tarea.
+
+
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public bool iscompleted { get; set; }
+
+        String title;
+        public String Title
+        {
+            get { return title; }
+            set
+            {
+                if (title != value)
+                {
+                    title = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+
+        public ICommand Save { protected set; get; }
+        public ICommand Get { protected set; get; }
+
+        private List<Taskmodels> Taskmodels;
+          
+        public TaskViewModel()
+        {
+            Tasks= new ObservableCollection<Taskmodels>();
+            Taskmodels = new List<Taskmodels>();
+            Save = new Command(() =>
+            {
+
+                Taskmodel task = new TaskModel();
+                task.Title = this.Title;
+                TaskViewModel.Add(task);
+            });
+            Get = new Command(() =>
+            {
+
+                Tasks = new ObservavleCollection<TaskModel>(Tas)
+
+
+
+            });
+
+    }
+}
